03470d995d21cd83930b62ba8fb7437be76415b7
OlyaGasanova <minnie.m759@gmail.com>
2017-08-10 20:24:01 +0300

    Update LoadModel.cs

--- LoadModel.cs
+++ LoadModel.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -46,18 +46,14 @@ public class LoadModel : MonoBehaviour
         renderer = CactusPart.AddComponent<MeshRenderer>();
         filter = CactusPart.AddComponent<MeshFilter>();
         filter.mesh = holderMesh;
+
         CactusPart.AddComponent<MeshCollider>().sharedMesh = null;
         CactusPart.GetComponent<MeshCollider>().sharedMesh = holderMesh;
         CactusPart.GetComponent<MeshCollider>().convex = true;
         //GetComponent<MeshCollider>().sharedMesh = null;
         // GetComponent<MeshCollider>().sharedMesh = holderMesh;
 
-        RaycastHit[] belowThings = GetComponent<Rigidbody>().SweepTestAll(-transform.up, 0.5f, QueryTriggerInteraction.Ignore);
-        Debug.Log("What you need");
-        foreach (RaycastHit item in belowThings)
-        {
-            Debug.Log(item);
-        }
+       
 
 
 
@@ -72,20 +68,7 @@ public class LoadModel : MonoBehaviour
 
     }
 
-    void OnCollisionStay(Collision collision)
-    {
 
-        Debug.Log("Ono rabotaet!!!");
-
-
-
-    }
-
-    void OnTriggerEnter(Collider collider)
-    {
-        // Вызывается, когда другой объект попадает в зону триггера
-        Debug.Log("Triggered by " + collider.gameObject.name);
-    }
 
     void Update()
     {
